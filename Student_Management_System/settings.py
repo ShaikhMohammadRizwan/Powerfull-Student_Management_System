0c1da8bea4f1e951b41e05ecce337311b779a5e9
Rizwan Shaikh Mohammad <127223616+ShaikhMohammadRizwan@users.noreply.github.com>
2024-11-22 21:39:55 +0530

    Update settings.py

--- Student_Management_System/settings.py
+++ Student_Management_System/settings.py
@@ -144,12 +144,7 @@ STATICFILES_DIRS = [
 
 # settings.py
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'brihjp6@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'LaZzy@@Rizwan'  # Replace with your email password or app-specific password
+
 
 AUTH_USER_MODEL = 'home_auth.CustomUser'  # Adjust 'home_auth' to the app where CustomUser is defined
 AUTHENTICATION_BACKENDS = (
